aaea609f9d12d2e722de1637a0ba5ef1a8a7cfe0
DavronbekMamarasulov07 <nilufar12345davron@icloud.com>
2025-04-13 17:04:50 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbopack: false,
-  },
   reactStrictMode: true,
   devIndicators: false,
   pageExtensions: ["js", "jsx"],
